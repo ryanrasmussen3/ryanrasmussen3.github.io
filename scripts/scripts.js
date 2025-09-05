82fa5039303269a72b11ac9d4d4c136d1102c18f
ryanrasmussen3 <ryanrasmussen3@mail.weber.edu>
2025-09-05 10:30:30 -0600

    dark and defult button working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,3 +8,25 @@ function lightMode() {
     document.getElementById('para3').style = "color: black;";
     document.getElementById('colors').innerHTML = "Light";
 }
+
+function darkMode() {
+    document.getElementById('mainPage').style = "background-color: black; color: white;";
+    document.getElementById('heading1').style = "background-color: black; color: white; border: 2px black;";
+    document.getElementById('heading2').style = "background-color: black; color: white; border: 2px black;";
+    document.getElementById('heading3').style = "background-color: black; color: white; border: 2px black;";
+    document.getElementById('para').style = "color: white;";
+    document.getElementById('para2').style = "color: white;";
+    document.getElementById('para3').style = "color: white;";
+    document.getElementById('colors').innerHTML = "Dark";
+}
+
+function defultMode() {
+    document.getElementById('mainPage').style = "background-color: #1E0100;";
+    document.getElementById('heading1').style = "background-color: #8A221D; color: #610A05; border: 2px #610A05; border-radius: 4px;";
+    document.getElementById('heading2').style = "background-color: #8A221D; color: #610A05; border: 2px #610A05; border-radius: 4px;";
+    document.getElementById('heading3').style = "background-color: #8A221D; color: #610A05; border: 2px #610A05; border-radius: 4px;";
+    document.getElementById('para').style = "color= #610A05";
+    document.getElementById('para2').style = "color= #610A05";
+    document.getElementById('para3').style = "color= #610A05";
+    document.getElementById('colors').innerHTML= "Defult";
+}
