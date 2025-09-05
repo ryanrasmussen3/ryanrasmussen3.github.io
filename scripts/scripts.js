f91cc8aa1c1122908d02e5922d57e682665ed4a0
ryanrasmussen3 <ryanrasmussen3@mail.weber.edu>
2025-09-05 10:02:14 -0600

    updated css and added three settings buttons with lightmode working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,10 @@
-/* Hello World! */
+function lightMode() {
+    document.getElementById('mainPage').style = "background-color: white; color: black;";
+    document.getElementById('heading1').style = "background-color: white; color: black; border: 2px black;";
+    document.getElementById('heading2').style = "background-color: white; color: black; border: 2px black;";
+    document.getElementById('heading3').style = "background-color: white; color: black; border: 2px black;";
+    document.getElementById('para').style = "color: black;";
+    document.getElementById('para2').style = "color: black;";
+    document.getElementById('para3').style = "color: black;";
+    document.getElementById('colors').innerHTML = "Light";
+}
